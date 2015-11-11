c79e7e44d4c685f932d3b86f440177f3766ee8dc
Eric Budd <eric.b.budd@gmail.com>
2015-11-11 15:21:42 -0600

    Adding config.sh; tweaks

---
+++ config.sh
@@ -0,0 +1,27 @@
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+
+sudo apt-get install build-essential cmake postgresql erlang git vim xmonad dmenu zsh python-dev tmux nodejs npm elixir vim-gnome ncurses-term
+
+mkdir Projects
+cd Projects
+git clone git@github.com:Calamitous/dotfiles.git
+ln -s /home/ericbudd/Projects/dotfiles/ /home/ericbudd/.
+
+ln -s /home/ericbudd/Projects/dotfiles/gitconfig /home/ericbudd/.gitconfig
+ln -s /home/ericbudd/Projects/dotfiles/jshintrc /home/ericbudd/.jshintrc
+ln -s /home/ericbudd/Projects/dotfiles/tmux.conf /home/ericbudd/.tmux.conf
+ln -s /home/ericbudd/Projects/dotfiles/tmuxinator /home/ericbudd/.tmuxinator
+ln -s /home/ericbudd/Projects/dotfiles/vimrc /home/ericbudd/.vimrc
+ln -s /home/ericbudd/Projects/dotfiles/zsh_aliases /home/ericbudd/.zsh_aliases
+ln -s /home/ericbudd/Projects/dotfiles/zshrc /home/ericbudd/.zshrc
+
+# Vundle install
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+cd ~/.vim/bundle/YouCompleteMe/
+./install.py
+
+git clone git://github.com/altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
+
+sudo dpkg -i ./<file>
+
--- zshrc
+++ zshrc
@@ -11,15 +11,15 @@ if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
 fi
 
 # Customize to your needs...
-eval "$(rbenv init - zsh --no-rehash)"
-source ~/.nvm/nvm.sh
+# eval "$(rbenv init - zsh --no-rehash)"
+# source ~/.nvm/nvm.sh
 unsetopt AUTO_CD
 
-source ~/.aws_conf
+# source ~/.aws_conf
 export EDITOR=vim
-export PATH=$PATH:~/Projects/grep-fu/bin
+# export PATH=$PATH:~/Projects/grep-fu/bin
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
-export PATH=$PATH:/Users/ericbudd/.rbenv/versions/2.2.0/lib/ruby/gems/2.2.0/gems/tmuxinator-0.6.10/bin
+# export PATH=$PATH:/Users/ericbudd/.rbenv/versions/2.2.0/lib/ruby/gems/2.2.0/gems/tmuxinator-0.6.10/bin
 
 # Turn off the stupid autocorrect malfeature
 unsetopt correct
@@ -30,3 +30,5 @@ source ~/.zsh_aliases
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
 bindkey "^X^E" edit-command-line
+
+export TERM=xterm-256color
